7336120a31fa1080317327cef5bca3fb2b5f73d2
Jane <jenya.kissel@gmail.com>
2026-01-21 12:27:01 +0100

    performing sentiment analysis using different models, standardizing sentiment labels across models for comparison, plotting negative sentiment distribution overlap between models in a Venn diagram

--- News-Sentiment-Analysis.py
+++ News-Sentiment-Analysis.py
@@ -28,7 +28,7 @@ tesla_articles = dataset['train'].filter(lambda example: 'Tesla' in example['doc
 tesla_df = pd.DataFrame(tesla_articles)
 
 #print(tesla_df.head(10))
-print(f"Total articles mentioning Tesla: {len(tesla_df)}")
+print(f"Total articles mentioning Tesla: {len(tesla_df)}") # Total articles mentioning Tesla: 210
 
 # Initialize sentiment analysis pipeline
 def text_classification(model_name, text):
@@ -39,9 +39,44 @@ def text_classification(model_name, text):
     pipe = pipeline("text-classification", model=model_name)
 
     # Analyze texts sentiment
-    result = pipe(text['article'], truncation=True, max_length = 512)
+    result = pipe(text['article'], truncation=True, max_length = 512, batch_zize=16) # adding a batch size for faster processing
 
     # Clean up resources
-    del pipe
+    del pipe # Deleting pipe from object memrory to clean up GPU/CPU space
 
     return pd.DataFrame(result)
+
+# Perform sentiment analysis on Tesla articles using dffferent models
+finbert_result = text_classification("ProsusAI/finbert", tesla_articles)
+distilbert_result = text_classification("distilbert/distilbert-base-uncased", tesla_articles)
+bert_base_result = text_classification("bert-base-uncased", tesla_articles)
+deepseek_result = text_classification("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", tesla_articles)
+
+# Harmonize sentiment labels across the models for comparison
+bert_base_result['label'].replace(['1 star', '2 stars'], 'NEGATIVE', inplace=True)
+bert_base_result['label'].replace(['4 star', '5 stars'], 'POSITIVE', inplace=True)
+bert_base_result['label'].replace(['3 stars'], 'NEUTRAL', inplace=True)
+
+distilbert_result['label'].replace(['LABEL_0'], 'NEGATIVE', inplace=True)
+distilbert_result['label'].replace(['LABEL_1'], 'POSITIVE', inplace=True)
+
+deepseek_result['label'].replace(['LABEL_0'], 'NEGATIVE', inplace=True)
+deepseek_result['label'].replace(['LABEL_1'], 'POSITIVE', inplace=True)
+
+# Plot negative sentiment distribution overlap for each model
+finbert_negative = finbert_result[finbert_result['label'] == 'NEGATIVE'].index.tolist()
+distilbert_negative = distilbert_result[distilbert_result['label'] == 'NEGATIVE'].index.tolist()
+bert_base_negative = bert_base_result[bert_base_result['lable'] == 'NEGATIVE'].index.tolist()
+deepseek_negative = deepseek_result[deepseek_result['label'] == 'NEGATIVE'].index.tolist()
+
+sets_negative = {
+    "FinBERT": set(finbert_negative),
+    "DistilBERT": set(distilbert_negative),
+    "BERT-Base": set(bert_base_negative),
+    "DeepSeek": set(deepseek_negative)
+}
+
+# Plot Venn diagram for negative sentiment overlap
+venny4py(sets = sets_negative)
+plt.title("Negative Sentiment Overlap Between Models")
+#plt.show()
