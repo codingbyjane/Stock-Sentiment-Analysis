3bdde77d8a7e37b13a9cb729d20be3ab7a0969fe
Jane <jenya.kissel@gmail.com>
2026-01-21 11:34:43 +0100

    filtering the dataset for only the articles mentioning Tesla, storing the results in a dataframe, defining a sentiment analysis function

--- News-Sentiment-Analysis.py
+++ News-Sentiment-Analysis.py
@@ -1,5 +1,5 @@
 # import ncessary components from transformers library
-from transformers import pipline, AutoTokenizer, set_seed
+from transformers import pipeline, AutoTokenizer, set_seed
 
 # import dataset library
 from datasets import load_dataset
@@ -20,3 +20,28 @@ import yfinance as yf
 
 # Load XSum dataset
 dataset = load_dataset('xsum')
+
+# Filter articles that mention 'Tesla'
+tesla_articles = dataset['train'].filter(lambda example: 'Tesla' in example['document'])
+
+# Convert filtered dataset to pandas DataFrame
+tesla_df = pd.DataFrame(tesla_articles)
+
+#print(tesla_df.head(10))
+print(f"Total articles mentioning Tesla: {len(tesla_df)}")
+
+# Initialize sentiment analysis pipeline
+def text_classification(model_name, text):
+    # Ensure reproducibility
+    set_seed(42)
+
+    # Initialize the sentiment analysis pipeline
+    pipe = pipeline("text-classification", model=model_name)
+
+    # Analyze texts sentiment
+    result = pipe(text['article'], truncation=True, max_length = 512)
+
+    # Clean up resources
+    del pipe
+
+    return pd.DataFrame(result)
